e38697d4073032bb696adb437b63410833950b06
JunggleVincent81 <tegarbagussantoso1116@gmail.com>
2026-06-17 17:14:38 +0700

    test

--- backend/seedAdmin.js
+++ backend/seedAdmin.js
@@ -6,7 +6,7 @@ const User = require("./src/models/User");
 
 const seedAdmin = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGO_URL);
 
     const existingAdmin = await User.findOne({
       email: "admin@premku.com",
--- backend/seedProducts.js
+++ backend/seedProducts.js
@@ -44,7 +44,7 @@ const products = [
 
 const seedProducts = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("✅ Connected to MongoDB");
 
